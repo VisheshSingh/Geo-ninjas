13d88543a5f51709eb748caa1381b1ee509aca91
Vishesh <visheshsingh16@gmail.com>
2018-10-21 10:20:50 -0400

    Updating a user's location

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -6,6 +6,7 @@
 
 <script>
 import firebase from "firebase";
+import db from "@/firebase/init";
 
 export default {
   name: "GMap",
@@ -27,12 +28,34 @@ export default {
     }
   },
   mounted() {
+    // get current user
+    let user = firebase.auth().currentUser;
+    // console.log(user);
     // get geolocation
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         pos => {
           (this.lat = pos.coords.latitude), (this.lng = pos.coords.longitude);
-          this.renderMap();
+
+          // find the user record then update the geolocation
+          db.collection("users")
+            .where("user_id", "==", user.uid)
+            .get()
+            .then(snapshot => {
+              snapshot.forEach(doc => {
+                db.collection("users")
+                  .doc(doc.id)
+                  .update({
+                    geolocation: {
+                      lat: pos.coords.latitude,
+                      lng: pos.coords.longitude
+                    }
+                  });
+              });
+            })
+            .then(() => {
+              this.renderMap();
+            });
         },
         err => {
           console.log(err);
